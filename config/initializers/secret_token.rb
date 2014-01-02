f3d94939fb07065164c71d08f5a18dbc998a0307
Connie <connieliang231@gmail.com>
2014-01-02 13:35:23 -0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Smpl::Application.config.secret_key_base = 'ee659f60499094853cfc576cef90ea7adbb50f55e8fdc51db634b52e2047f425328479818beffa1f0f265b14c6b4e385c9e293ecb5f7655671ec696ad3f52410'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Smpl::Application.config.session_store :cookie_store, key: '_smpl_session'
