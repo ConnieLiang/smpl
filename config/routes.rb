12afde1de2b93e62897ae4cf416d7cf2d59f8299
Connie <connieliang231@gmail.com>
2014-01-02 15:54:47 -0800

    Creating home and about pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Smpl::Application.routes.draw do
+  get "pages/home"
   root "pages#home"
   get "about" => "pages#about"
   # The priority is based upon order of creation: first created -> highest priority.
